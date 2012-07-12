2911cbf4b7bce638d006547e2bdff1c03c59113b
Paul Hooijenga <paulhooijenga@gmail.com>
2012-07-12 15:23:18 +0200

    fixed quotes in description

--- xdebug.fish
+++ xdebug.fish
@@ -1,4 +1,4 @@
-function xdebug --description 'Run a PHP Script with xdebug.remote_autostart on'
+function xdebug --description "Run a PHP Script with xdebug.remote_autostart on"
 	set file $argv[1]
 	set -e argv[1]
 	php -d xdebug.remote_autostart=On -d xdebug.remote_mode=req -d xdebug.remote_enable=1 -f "$file" -- $argv
