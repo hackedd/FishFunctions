581f7287a47b29f693cd2d6f2ddc354c97db1412
Paul Hooijenga <paulhooijenga@gmail.com>
2013-05-29 17:20:37 +0200

    added virtualenv: create virtualenv with colored prompt by default

---
+++ virtualenv.fish
@@ -0,0 +1,19 @@
+function virtualenv --description "Create virtualenv with colored prompt"
+	if test (count $argv) -lt 1
+		echo "Usage: virtualenv [OPTIONS] DEST_DIR [PROMPT]" >&2
+		return 1
+	end
+
+	if begin; contains -- "--prompt" $argv; or contains -- "-h" $argv; or contains -- "--help" $argv; end
+		command virtualenv $argv
+	else
+		set -l prompt
+		if test (count $argv) -lt 2
+			set prompt (basename (pwd))
+		else
+			set prompt $argv[-1]
+			set -e $argv[-1]
+		end
+		virtualenv $argv --prompt (set_color -b blue white)"($prompt)"(set_color normal)" "
+	end
+end
