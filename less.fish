065e493dcc7ef6e51338d639a05924d5419e7325
Paul Hooijenga <paulhooijenga@gmail.com>
2013-11-17 00:03:11 +0100

    use vim74/macros/less.sh if it exists

--- less.fish
+++ less.fish
@@ -1,9 +1,14 @@
 function less --description "use vim in less mode"
-	if count $argv >/dev/null
-		set args $argv
-	else
-		set args "-"
-	end
+    if test -f /usr/share/vim/vim74/macros/less.sh
+        /usr/share/vim/vim74/macros/less.sh $argv
+        return
+    end
 
-	vim -u /usr/share/vim/vimcurrent/macros/less.vim $args
+  if count $argv >/dev/null
+    set args $argv
+  else
+    set args "-"
+  end
+
+  vim -u /usr/share/vim/vimcurrent/macros/less.vim $args
 end
