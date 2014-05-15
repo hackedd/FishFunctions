010535e5037588d5043986b1aa497a0a471382a2
Paul Hooijenga <paul.hooijenga@sanoma.com>
2014-05-15 12:35:19 +0200

    fix less function to support multiple vim versions

--- less.fish
+++ less.fish
@@ -1,14 +1,19 @@
 function less --description "use vim in less mode"
-    if test -f /usr/share/vim/vim74/macros/less.sh
-        /usr/share/vim/vim74/macros/less.sh $argv
-        return
+  if not functions -q __less
+    for ver in current 74 73
+      set -l lesssh "/usr/share/vim/vim$ver/macros/less.sh"
+      if test -f $lesssh
+        echo "function __less; $lesssh \$argv; end" | .
+        break
+      end
     end
-
-  if count $argv >/dev/null
-    set args $argv
-  else
-    set args "-"
   end
 
-  vim -u /usr/share/vim/vimcurrent/macros/less.vim $args
+  if not functions -q __less
+    function __less
+      command less $argv
+    end
+  end
+
+  __less $argv
 end
