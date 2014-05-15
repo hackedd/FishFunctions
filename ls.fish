22ad43b0ef2031c9103ae5be76b63c6e32b6c932
Paul Hooijenga <paul.hooijenga@sanoma.com>
2014-05-15 12:34:22 +0200

    fix ls flags for OS X

--- ls.fish
+++ ls.fish
@@ -1,7 +1,11 @@
 function ls --description "List contents of directory"
-	set -l param -h --color=auto
+	set -l param -h
 	if isatty 1
-		set param $param --indicator-style=classify
+		if [ (uname) = "Darwin" ]
+			set param $param -G -F
+		else
+			set param $param --color=auto --indicator-style=classify
+		end
 	end
 	command ls $param $argv
 end
