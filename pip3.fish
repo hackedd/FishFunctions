5823c0205675e7b52aaa38194586a6a0968a5ff4
Paul Hooijenga <paulhooijenga@gmail.com>
2016-09-08 11:24:27 +0200

    add pip3

--- pip3.fish
+++ pip3.fish
@@ -1,9 +1,9 @@
-# When you run "pip install" in your regular shell outside a
+# When you run "pip3 install" in your regular shell outside a
 # virtualenv, it should just work, meaning, install stuff into your
 # home directory.  But, let’s not affect the operation of pip within
 # virtualenvs or as executed by any tools (such as tox or virtualenv
 # itself) which need to run it via automation and not via a shell.
-function pip
+function pip3
     if not test -n "$PYTHON_USER_BIN"
         set -xg PYTHON_USER_BIN (python -m site --user-base)/bin
         set -xg PYTHON_USER_LIB (python -m site --user-site)
@@ -11,8 +11,8 @@ function pip
     end
 
     if test -n "$VIRTUAL_ENV"
-        command pip $argv
+        command pip3 $argv
     else
-        env PIP_USER=yes pip $argv
+        env PIP_USER=yes pip3 $argv
     end
 end
