530ef22759ca73b56483cfead669e7d32d951608
Paul Hooijenga <paulhooijenga@gmail.com>
2016-11-10 20:01:05 +0100

    Add which

---
+++ which.fish
@@ -0,0 +1,30 @@
+function which --description "which, but with more info"
+    set -l count (count $argv)
+    for arg in $argv
+        set -l path (command which $arg)
+        if test $status -ne 0
+            if test $count -gt 1
+                echo -n "$arg: " >&2
+            end
+            echo "not found" >&2
+        else
+            if test $count -gt 1
+                echo -n "$arg: "
+            end
+            echo "$path"
+
+            while test -L "$path"
+                # Get the target of the link
+                set -l target (readlink "$path")
+
+                # Resolve the link target relative to the directory of the link
+                pushd (dirname "$path") 2>/dev/null
+                set -l path (realpath -s "$target")
+                popd 2>/dev/null
+
+                echo " -> $path"
+            end
+        end
+    end
+    return 0
+end
