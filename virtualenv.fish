bf2714261128db62fe2a031cbc576a27ffe7da7f
Paul Hooijenga <paul.hooijenga@sanoma.com>
2016-11-28 12:17:18 +0100

    add virtualenv3

--- virtualenv.fish
+++ virtualenv.fish
@@ -1,19 +1,13 @@
 function virtualenv --description "Create virtualenv with colored prompt"
     if test (count $argv) -lt 1
-        echo "Usage: virtualenv [OPTIONS] DEST_DIR [PROMPT]" >&2
+        echo "Usage: virtualenv [OPTIONS] DEST_DIR" >&2
         return 1
     end
 
     if begin; contains -- "--prompt" $argv; or contains -- "-h" $argv; or contains -- "--help" $argv; or contains -- "--version" $argv; end
         command virtualenv $argv
     else
-        set -l prompt
-        if test (count $argv) -lt 2
-            set prompt (basename (pwd))
-        else
-            set prompt $argv[-1]
-            set -e argv[-1]
-        end
+        set -l prompt (basename (pwd))
         command virtualenv $argv --prompt (set_color -b blue white)"($prompt)"(set_color normal)" "
         if activate
             pip install --upgrade pip setuptools
---
+++ virtualenv3.fish
@@ -0,0 +1,9 @@
+function virtualenv3 --description "Create Python 3 virtualenv with colored prompt"
+    if test (count $argv) -lt 1
+        echo "Usage: virtualenv [OPTIONS] DEST_DIR [PROMPT]" >&2
+        return 1
+    end
+
+    virtualenv --python (command which python3.5) $argv
+    return $status
+end
