eed771f3f02b35558987c5e5f01c8dfdf624b480
Paul Hooijenga <paulhooijenga@gmail.com>
2023-01-04 09:56:08 +0100

    Rewrite format_duration using builtins

--- format_duration.fish
+++ format_duration.fish
@@ -1,18 +1,14 @@
 function format_duration
-    set -l seconds (math $argv[1] / 1000)
-    set -l format
-    if test $seconds -gt 3600
-        set format "+%-Hh %-Mm %-Ss"
-    else if test $seconds -gt 60
-        set format "+%-Mm %-Ss"
+    set -l total_seconds (math $argv[1] / 1000)
+    set -l hours (math -s0 "$total_seconds / 3600")
+    set -l minutes (math -s0 "($total_seconds % 3600) / 60")
+    set -l seconds (math -s0 "$total_seconds % 60")
+
+    if test $total_seconds -gt 3600
+        printf "%dh %dm %ds\n" $hours $minutes $seconds
+    else if test $total_seconds -gt 60
+        printf "%dm %ds\n" $minutes $seconds
     else
-        set format "+%-Ss"
+        printf "%ds\n" $seconds
     end
-
-    set -l date date
-    if [ (uname) = "Darwin" ]
-        set date gdate
-    end
-
-    eval "$date -u -d \"@$seconds\" \"$format\""
 end
